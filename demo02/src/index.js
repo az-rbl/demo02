d50a785df8e4db6b7e8f531e62059d52056d56ed
raul <192793@iberopuebla.mx>
2022-09-28 14:32:54 -0500

    condicionales y maping

--- demo02/src/index.js
+++ demo02/src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const name = 'Raúl'
+const sesion = true;
+const  apellido = ''
+const alumnos =["juan", "pedro", "aldo"]
+const jsx =(<>
+{sesion === true ?(
+  <>
+<h1 className='titulo' style={{color:'red', fontSize:'4rem'}}>Hola {name}</h1>
+{apellido && <p>tu apellido es : {apellido}</p>}
+<h3>Lista de alumnos</h3>
+<ul>
+  {alumnos.map((nombre,i)=>{return<li key ={i}>{nombre}</li>})}
+</ul>
+</>
+):(<h2>NO has iniciado sesion</h2>)}
+</>
+);    
+
+
+
+// const compruebaSesion = (sesion) => {
+//   if (sesion ===  true){return jsx}
+//   else{
+//     return <h2>NO has iniciado sesion</h2>
+//   }
+// }
+// root.render(compruebaSesion(sesion));
+
+
+
+root.render(jsx);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
